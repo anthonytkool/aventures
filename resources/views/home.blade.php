97c5129e18a49ada1f45aba727e29ca857b609e3
Tcool <anthonytcool@hotmail.com>
2025-05-18 11:42:27 +0700

    Initial full Laravel project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!-- resources/views/home.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AventureTrip</title>
+</head>
+<body>
+    <h1>Welcome to AventureTrip!</h1>
+    <p>Your Laravel travel website is working!</p>
+</body>
+</html>
